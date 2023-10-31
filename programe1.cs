6132eaba02e2617752456152c9b2f7f6227e4233
Nati <tusikf@yandex.ru>
2023-10-31 21:25:29 +0300

    done

--- programe1.cs
+++ programe1.cs
@@ -7,4 +7,63 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-Console.WriteLine("Введите исходный массив")
+Console.Write("Введите размер исходного массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+string[] array = GetArray(size);
+Console.WriteLine($"Исходный массив: [{String.Join(", ", array)}]");
+
+int j = 0;
+int schet = 0;
+int z = 0;
+string x = " ";
+
+
+string[] otvet = new string[size];
+for (int k = 0; k < size; k++)
+{
+    x = array[k];
+    int summ = 0;
+    foreach(int el in x)
+    {
+        summ++;
+    }
+
+    //Console.WriteLine($"элемент  массива: [{x}]");
+    if ( summ < 4)
+    {
+        otvet[j] = array[k];
+        j++;
+
+    }
+    else 
+    {
+        otvet[j] = "=";
+        j++;
+        schet++;
+
+    }
+}
+string[] otvet2 = new string[size-schet];
+for (int k = 0; k < size; k++)
+{
+    if (otvet[k]!="=")
+    {
+        otvet2[z]=otvet[k];
+        z++;
+    }
+}
+
+Console.WriteLine($"Итоговый массив где элементы короче или = 3 : [{String.Join(", ", otvet2)}]");
+
+// Функция заполнения массива
+string[] GetArray(int size)
+{
+    string[] result = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент исходного массива: ");
+        result[i] = Console.ReadLine()!;
+    }
+    return result;
+}
